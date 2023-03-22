e0423f53ed9b399ed0635815f17b8dfc25c7678c
Giorgio Grassini <giorgiograssini@gmail.com>
2023-03-22 22:42:26 +0100

    Added comments in index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+// Require the lodash library here in this line
+
+function sum(a, b) {
+  // Use de add() function from lodash to sum the two numbers
+}
+
+function filterFalsyValues(arr) {
+  // Use the compact() function from lodash to filter the array
+}
+
+function differenceInArrays(firstArray, secondArray) {
+  // Use the difference() function from lodash to get the difference between the two arrays
+}
+
+function fillArray(arr, value) {
+  // Use the fill() function from lodash to fill the array with the value
+}
+
+function calculateMean(arr) {
+  // Use the mean() function from lodash to calculate the mean of the array
+}
+
+function camelCase(str) {
+  // Use the camelCase() function from lodash to convert the string to camelCase
+}
+
+module.exports = {
+  sum,
+  filterFalsyValues,
+  differenceInArrays,
+  fillArray,
+  calculateMean,
+  camelCase,
+};
