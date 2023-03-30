96618df4c1eccd04568cee341eaa806146d38092
OmarjLand <omar.land2010@gmail.com>
2023-03-30 20:13:11 +0200

    Finished the last 3 tests

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function sum(a, b) {
   let addition = _.add(a, b) 
   return addition;
 }
-console.log( sum( 1,2 ) );
+//console.log( sum( 1,2 ) );
 
 // 2 ✅
 let array = [1,2,'','A','z',3,4];
@@ -16,7 +16,7 @@ function filterFalsyValues(arr) {
   let compacting = _.compact( arr );
   return compacting;
 }
-console.log( filterFalsyValues( array ) );
+//console.log( filterFalsyValues( array ) );
 
 // 3 ✅
 let arrayA = [1,2];
@@ -26,7 +26,7 @@ function differenceInArrays(firstArray, secondArray) {
   let difference = _.difference( firstArray, secondArray );
   return difference;
 }
-console.log( differenceInArrays( arrayA, arrayB ) );
+//console.log( differenceInArrays( arrayA, arrayB ) );
 
 // 4 ✅
 let arrayToFill = ['a','b','c'];
@@ -35,7 +35,7 @@ function fillArray(arr, value) {
   let filled = _.fill( arr, value );
   return filled;
 }
-console.log( fillArray( arrayToFill, 5 ) );
+//console.log( fillArray( arrayToFill, 5 ) );
 
 // 5 ✅
 let arrayC = [2,4,8,9,10]
@@ -44,7 +44,7 @@ function calculateMean(arr) {
   let showMean = _.mean( arr );
   return showMean;
 }
-console.log( calculateMean(arrayC) );
+//console.log( calculateMean(arrayC) );
 
 // 6 ✅
 let camellingString = "hola soy tu padre"
@@ -53,7 +53,7 @@ function camelCase(str) {
   let camelled = _.camelCase( str );
   return camelled;
 }
-console.log( camelCase( camellingString ) );
+//console.log( camelCase( camellingString ) );
  
 // 7 ✅
 let capitalizeName = "LANDAETA";
@@ -62,7 +62,7 @@ function capitalize(str) {
   let showCapi = _.capitalize( str );
   return showCapi;
 }
-console.log( capitalize( capitalizeName ) );
+//console.log( capitalize( capitalizeName ) );
 
 // 8 ✅
 const superheroes = [
@@ -74,25 +74,43 @@ function sortByNames(arr) {
   const object = _.sortBy( arr, [ (h) => h.name ] );
   return object;
 }
-console.log( sortByNames(superheroes) );
+//console.log( sortByNames(superheroes) );
 
-// 9
+// 9 ✅
+let arrayD = [1,[2,[3,4,5],6]];
 function flatArrayAndCalculateMean(arr) {
   // Use the mean() function from lodash to calculate the mean of the array
   // Use the flatten() function from lodash to flatten the array
-}
+  let flatting = _.flatten( arr );
+  let makeMean = _.mean( flatting );
 
-// 10
+  return makeMean;
+}
+//console.log( flatArrayAndCalculateMean( arrayD ) );
+
+// 10 ✅
+let arrayE = [
+  { name  : 'john'},
+  { name  : 'alex'},
+];
 function sortByNamesAndCapitalizeFirstLetter(arr) {
   // Use the sortBy() function from lodash to sort the array by name
   // Use the capitalize() function from lodash to capitalize the first letter of each name
+  let sortingArray = _.sortBy( arr, ( n ) => n.name );
+  let capi = sortingArray.map( n => _.capitalize( n.name ) );
+
+  return capi;
 }
+//console.log( sortByNamesAndCapitalizeFirstLetter( arrayE ) );
 
 // 11
+let greets = ["Hello world"];
 function transformToSnakeCaseAndAllCaps(arr) {
   // Use the snakeCase() function from lodash to convert the string to snake_case
   // Use the toUpper() function from lodash to convert the string to uppercase
+  return arr.map( (str) => _.toUpper(_.snakeCase(str)));
 }
+//console.log( transformToSnakeCaseAndAllCaps( greets ) );
 
 module.exports = {
   sum,
