dd32f639ea4c2e491ef184631cf6eb1c0f1a3124
Cristina <c.gomezlop95@gmail.com>
2023-11-14 16:14:23 +0100

    all functions done

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Require the lodash library here in this line
-//import lodash from "lodash"; 
 const lodash = require("lodash");
 
 function sum(a, b) {
@@ -37,16 +36,10 @@ function capitalize(str) {
   return lodash.capitalize([str]);
 }
 
-//let myString = "hello world";
-//console.log(capitalize(myString)); //Hello world
-
 function sortByNames(arr) {
   // Use the sortBy() function from lodash to sort the array by name
-  return lodash.sortBy(arr, ['name']);
+  return lodash.sortBy(arr, ["name"]);
 }
-
-//let myArrayOfObjects = [{ name: "John" }, { name: "Alex" }];
-//console.log(sortByNames(myArrayOfObjects)); //[ { name: 'Alex' }, { name: 'John' } ]
 
 function flatArrayAndCalculateMean(arr) {
   // Use the mean() function from lodash to calculate the mean of the array
@@ -55,32 +48,23 @@ function flatArrayAndCalculateMean(arr) {
   return lodash.mean(flattenArray);
 }
 
-//let myArray = [
-  //[1, 2],
-  //[3, 4],
-//];
-//console.log(flatArrayAndCalculateMean(myArray)); //2.5
-
 function sortByNamesAndCapitalizeFirstLetter(arr) {
   // Use the sortBy() function from lodash to sort the array by name
   // Use the capitalize() function from lodash to capitalize the first letter of each name
-  let arraySortedByName = lodash.sortBy(arr, ['name']); //[ { name: 'alex' }, { name: 'john' } ]
-  let arrayOnlyValues = arraySortedByName.map(element => Object.values(element)); //[ [ 'alex' ], [ 'john' ] ]
+  let arraySortedByName = lodash.sortBy(arr, ["name"]); //[ { name: 'alex' }, { name: 'john' } ]
+  let arrayOnlyValues = arraySortedByName.map((element) =>
+    Object.values(element)
+  ); //[ [ 'alex' ], [ 'john' ] ]
   let arrayFlattened = lodash.flatten(arrayOnlyValues); //[ 'alex', 'john' ]
-  return arrayFlattened.map(element => lodash.capitalize([element])); //[ 'Alex', 'John' ]
+  return arrayFlattened.map((element) => lodash.capitalize([element])); //[ 'Alex', 'John' ]
 }
-
-//let myArray = [
-  //{ name: "john" },
-  //{ name: "alex" },
-//];
-//console.log((sortByNamesAndCapitalizeFirstLetter(myArray)));
-
-//toEqual(["Alex", "John"]
 
 function transformToSnakeCaseAndAllCaps(arr) {
   // Use the snakeCase() function from lodash to convert the string to snake_case
   // Use the toUpper() function from lodash to convert the string to uppercase
+  let snakeString = lodash.snakeCase([arr]); //hello_world
+  let finalString = lodash.toUpper(snakeString); // HELLO_WORLD
+  return finalString.split(" "); //[ 'HELLO_WORLD' ]
 }
 
 module.exports = {
